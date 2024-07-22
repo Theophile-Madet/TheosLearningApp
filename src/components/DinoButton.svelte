735e037e9d3c05ebf29f7f5f0e47e0ce6370a271
Théophile Madet <theo.madet@posteo.net>
2024-07-22 20:25:53 +0200

    Better spinner color

--- src/components/DinoButton.svelte
+++ src/components/DinoButton.svelte
@@ -1,15 +1,17 @@
 <script lang="ts">
 
 
-	import { Button, Icon, Spinner } from '@sveltestrap/sveltestrap';
+	import { Button, Icon, Spinner, type TextColor } from '@sveltestrap/sveltestrap';
 
 	export let text: string;
-	export let color: string;
+	export let color: TextColor;
 	export let disabled: boolean = false;
 	export let icon: string = '';
 	export let type: string = 'button';
 	export let loading: boolean = false;
 	export let outline: boolean = false;
+
+	let spinnerColor = (color + '-emphasis') as TextColor;
 </script>
 
 <Button on:click
@@ -21,7 +23,7 @@
 		<Icon name="{icon}" />
 	{/if}
 	{#if loading}
-		<Spinner type="border" color="secondary" size="sm" />
+		<Spinner type="border" color="{spinnerColor}" size="sm" />
 	{/if}
 	{text}
 </Button>
