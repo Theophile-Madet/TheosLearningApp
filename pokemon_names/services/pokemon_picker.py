9704dcafc7c367ed8851851cb0a2561bab77c20c
Théophile Madet <theo.madet@posteo.net>
2025-02-14 14:32:56 +0100

    Pokemon pickers tries to take pokemons from early in the list.

--- pokemon_names/services/pokemon_picker.py
+++ pokemon_names/services/pokemon_picker.py
@@ -1,8 +1,14 @@
+import datetime
+import random
+
 from django.contrib.auth.models import User
 from django.db.models import Q
 from django.db.models.aggregates import Count
+from django.utils import timezone
 
 from content.models import Pokemon
+from learning.apps import LearningConfig
+from learning.models import PokemonNameResult
 from pokemon_names.services.generation_options_provider import GenerationOptionsProvider
 from pokemon_names.services.language_options_provider import LanguageOptionsProvider
 
@@ -14,10 +20,10 @@ class PokemonPicker:
         if not enabled_generations:
             raise ValueError("No enabled generation found")
 
-        pokemons = Pokemon.objects.filter(generation__in=enabled_generations)
+        potential_pokemons = Pokemon.objects.filter(generation__in=enabled_generations)
 
         enabled_languages = LanguageOptionsProvider.get_enabled_languages(user)
-        pokemons = pokemons.annotate(
+        potential_pokemons = potential_pokemons.annotate(
             nb_learned=Count(
                 "learnedpokemonname",
                 filter=Q(
@@ -27,7 +33,19 @@ class PokemonPicker:
             )
         ).filter(nb_learned__lt=len(enabled_languages))
 
-        picked_pokemon = pokemons.order_by("?").first()
+        pokemons_tried_in_the_last_24_hours = PokemonNameResult.objects.filter(
+            user=user, created_at__gte=timezone.now() - datetime.timedelta(days=1)
+        ).values_list("pokemon_id")
+
+        potential_pokemons = potential_pokemons.exclude(
+            id__in=pokemons_tried_in_the_last_24_hours
+        )
+
+        potential_pokemons = potential_pokemons.order_by("id")[
+            : LearningConfig.POOL_SIZE
+        ]
+
+        picked_pokemon = random.choice(potential_pokemons)
         if not picked_pokemon:
             raise ValueError(
                 f"No pokemon found for {user.username} "
